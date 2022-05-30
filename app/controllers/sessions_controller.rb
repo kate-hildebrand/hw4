adf106c7c902c9e43e1b385e97e417f5345d9719
kate-hildebrand <kate.hildebrand@kellogg.northwestern.edu>
2022-05-30 18:03:11 +0000

    login works w "create"

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,9 @@ class SessionsController < ApplicationController
     # authenticate the user
     @user = User.find_by({ "email" => params["email"] })
     if @user
-      if BCrypt::Password.new(@user["password"]) == params["password"]
+      if BCrypt::Password.create(@user["password"]) == params["password"]
         session["user_id"] = @user["id"]
-        flash["notice"] = "Welcome, #{@user["first_name"]}."
+        flash["notice"] = "Welcome, #{@user["username"]}."
         redirect_to "/places"
       else
         flash["notice"] = "Login Failed"
