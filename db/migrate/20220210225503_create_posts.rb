89feaa39d61eb9a0d94699174dff32254983466d
kate-hildebrand <kate.hildebrand@kellogg.northwestern.edu>
2022-06-07 02:54:48 +0000

    all done

--- db/migrate/20220210225503_create_posts.rb
+++ db/migrate/20220210225503_create_posts.rb
@@ -2,12 +2,11 @@ class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
       t.string "title"
-      t.string "image"
       t.text "description"
       t.date "posted_on"
       t.integer "place_id"
       t.integer "user_id"
-
+      t.string "image"
       t.timestamps
     end
   end
